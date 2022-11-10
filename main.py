38e229d3cd6ecd2b4ca168a4bf56152e4952c3bf
jademnchx <jade.mnchx@gmail.com>
2022-11-10 10:01:00 +0100

    master

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ import sys
 
 def main() :
     print("Hello World")
+    T = [download_image]
+    P = [download_image]
+    G = [download_image]
+    for i in range(100):
+    for i in range(len(T)):
+        T[i].start()
+    for i in range(len(T)):
+        T[i].join()
+    
 if __name__ == "__main__":
     sys.exit(0)
