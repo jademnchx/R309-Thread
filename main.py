1bd6ee2d62983a7be868fec43df6fa7378778f8a
jademnchx <jade.mnchx@gmail.com>
2022-11-10 09:55:03 +0100

    master

---
+++ main.py
@@ -0,0 +1,9 @@
+from exo_6 import download_image
+from exo_valid_1 import download_image
+from exo_valid_1_3 import download_image
+import sys 
+
+def main() :
+    print("Hello World")
+if __name__ == "__main__":
+    sys.exit(0)
