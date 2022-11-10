78c8a5fa33f72154a4a3ca496ed4e4320692fbb3
jademnchx <jade.mnchx@gmail.com>
2022-11-10 10:43:49 +0100

    master

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from exo_6 import download_image
-from exo_valid_1 import download_image
-from exo_valid_1_3 import download_image
+from thread_6_pool import download_image as pool
+from thread_9_threadingthread import download_image as thread
+from threas_8_multipsocess import download_image as multiprocessing
 import sys 
-
+#Thread
 def main() :
     print("Hello World")
-    T = [download_image]
-    P = [download_image]
-    G = [download_image]
+    T = [pool]
+    P = [thread]
+    G = [multiprocessing]
     for i in range(100):
+        
     for i in range(len(T)):
         T[i].start()
     for i in range(len(T)):
