6a1cf45ec60e6de384a77d29d5d14cba7828fc0c
jademnchx <jade.mnchx@gmail.com>
2022-11-28 09:24:59 +0100

    jm

--- thread_7.py
+++ thread_7.py
@@ -4,7 +4,7 @@ def task():
     print(f"Task starts for 1 second")
     time.sleep(1)
     print(f"Task ends")
-    
+
 if __name__ == '__main__':
     start = time.perf_counter()
     p1 = multiprocessing.Process(target=task)
